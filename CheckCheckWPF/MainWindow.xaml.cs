a2a091de6f19e364fa84eaaf18d97f132ec99d25
terje <terje@MBP-WIN>
2016-07-05 21:04:50 +0200

    InitalCommit

--- CheckCheckWPF/MainWindow.xaml.cs
+++ CheckCheckWPF/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace CheckCheckWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int counter;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnRescanFolder_Click(object sender, RoutedEventArgs e)
+        {
+            
+            WrapPanel newWrapPanel = CreateNewWrapPanel();
+            spShowResult.Children.Add(newWrapPanel);
+            
+        }
+
+        private WrapPanel CreateNewWrapPanel()
+        {
+            counter++;
+            WrapPanel w = new WrapPanel { Name = "myWrapPanel" + counter, Margin = new Thickness(6) };
+            TextBlock tb = new TextBlock { Text = "Dette er tekst nummer: " + counter, Name = "textblock" + counter };
+            Button btn = new Button
+            {
+                Content = "Fjern rad nummer " + counter,
+                Name = "button" + counter,
+                Margin = new Thickness(10, 0, 10, 0)
+            };
+            CheckBox chk = new CheckBox { Content = "Dette er sjekkboks: " + counter, Name = "checkbox" + counter };
+
+            // Event for klikk
+            btn.Click += btn_Click;
+
+            w.Children.Add(tb);
+            w.Children.Add(btn);
+            w.Children.Add(chk);
+
+            return w;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button objButton = (Button)sender;
+            WrapPanel objWrap = (WrapPanel)objButton.Parent;
+
+            MessageBox.Show(objWrap.Name.ToString() + " ble fjernet.");
+
+            spShowResult.Children.Remove(objWrap);
+
+
+        }
+
+        private void btnCheckAllEps_Click(object sender, RoutedEventArgs e)
+        {
+            lboxShowFiles.Items.Add("Ny tekst");
         }
     }
 }
